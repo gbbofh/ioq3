647f72014edb118562175f5fa65ab25e95fd5fe2
Garrett Bates <garrett.bates@alumni.nmt.edu>
2021-09-03 13:14:09 -0600

    Testing normals with model matrix

--- code/renderergl2/tr_shade.c
+++ code/renderergl2/tr_shade.c
@@ -145,7 +145,7 @@ static void DrawNormals (shaderCommands_t *input) {
 
 		GLSL_BindProgram(sp);
 		
-		GLSL_SetUniformMat4(sp, UNIFORM_MODELMATRIX, glState.modelview);
+		GLSL_SetUniformMat4(sp, UNIFORM_MODELMATRIX, backEnd.or.transformMatrix);
 		/*VectorSet4(color, 1, 1, 1, 1);
 		GLSL_SetUniformVec4(sp, UNIFORM_COLOR, color);*/
 		GLSL_SetUniformInt(sp, UNIFORM_ALPHATEST, 0);
