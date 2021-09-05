6e442a9d6424205f74458efd5235c6948f1ce250
Garrett Bates <garrett.bates@alumni.nmt.edu>
2021-09-05 12:54:33 -0600

    Fixed typo in CCTV shader

--- code/renderergl2/glsl/cctv_fp.glsl
+++ code/renderergl2/glsl/cctv_fp.glsl
@@ -4,7 +4,7 @@ varying vec2   var_ScreenTex;
 
 void main()
 {
-    vec4 color = texture2D(u_ScreenColorMap, var_ScreenTex);
+    vec4 color = texture2D(u_ScreenImageMap, var_ScreenTex);
     gl_FragColor = color * vec4(1.0, 0.0, 0.0, 1.0);
 }
 
