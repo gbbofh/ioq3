db39046814ae74a3d7ab7a4c7d7350fcc97c4448
Garrett Bates <garrett.bates@alumni.nmt.edu>
2021-09-05 04:28:45 -0600

    Renamed uniforms to reflect changes in tr_glsl.c and tr_shade.c

--- code/renderergl2/glsl/normalcolor_fp.glsl
+++ code/renderergl2/glsl/normalcolor_fp.glsl
@@ -6,6 +6,9 @@ in vec3 v_Normal;
 
 void main()
 {
-    vec4 texSample = texture2D(u_DiffuseMap, varTex1);
-	gl_FragColor = texSample * vec4(v_Normal, 1);
+    vec3 normColor = v_Normal + vec3(1, 1, 1);
+    normColor += 0.5;
+
+    vec4 texSample = texture2D(u_DiffuseMap, var_Tex1);
+    gl_FragColor = texSample * vec4(normColor, 1);
 }
