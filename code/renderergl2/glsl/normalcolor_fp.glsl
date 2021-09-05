f69d956d015d14aaba8d0ace79b0d1a5c3b77d4f
Garrett Bates <garrett.bates@alumni.nmt.edu>
2021-09-05 03:37:35 -0600

    Debugging normal shader

--- code/renderergl2/glsl/normalcolor_fp.glsl
+++ code/renderergl2/glsl/normalcolor_fp.glsl
@@ -1,7 +1,5 @@
 in vec3 v_Normal;
 
-uniform sampler2D u_DiffuseMap;
-
 void main()
 {
 	gl_FragColor = vec4(attr_Normal, 1);
--- code/renderergl2/glsl/normalcolor_vp.glsl
+++ code/renderergl2/glsl/normalcolor_vp.glsl
@@ -8,4 +8,5 @@ out vec3 v_Normal;
 void main()
 {
 	gl_Position = u_ModelViewProjectionMatrix * vec4(attr_Position, 1.0);
+	v_Normal = attr_Normal;
 }
