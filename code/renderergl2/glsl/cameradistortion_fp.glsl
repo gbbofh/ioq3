430fdf831d0c5fc561013483b4fb7e032f21a6ac
Garrett Bates <garrett.bates@alumni.nmt.edu>
2021-09-07 03:16:44 -0600

    Testing pixelation effect

--- code/renderergl2/glsl/cameradistortion_fp.glsl
+++ code/renderergl2/glsl/cameradistortion_fp.glsl
@@ -4,5 +4,24 @@ varying vec2   var_ScreenTex;
 
 void main()
 {
+    const int PIXEL_SIZE = 2;
+
+    vec2 texSize = textureSize(u_ScreenImageMap).xy;
+
+    float x = int(gl_FragCoord.x) % PIXEL_SIZE;
+    float y = int(gl_FragCoord.y) % PIXEL_SIZE;
+
+    vec4 sample = texture(u_ScreenImageMap, gl_FragCoord.xy / texSize);
+
+    if(!sample.a) return;
+
+    x = floor(x / 2) - x;
+    y = floor(y / 2) - y;
+
+    x = x + gl_FragCoord.x;
+    y = y + gl_FragCoord.y;
+
+    sample = texture(u_ScreenImageMap, vec2(x, y) / texSize);
+
 	gl_FragColor = texture(u_ScreenImageMap, var_ScreenTex);
 }
